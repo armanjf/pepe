338620fe27e07b7e0bc131b05bb2f518ad3e5ca2
armanjf <63321986+armanjf@users.noreply.github.com>
2021-07-06 12:34:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+
+require_once "Controladores/plantillaC.php";
+
+require_once "Controladores/secretariasC.php";
+require_once "Modelos/secretariasM.php"; 
+
+
+require_once "Controladores/consultoriosC.php";
+require_once "Modelos/consultoriosM.php"; 
+
+require_once "Controladores/doctoresC.php";
+require_once "Modelos/doctoresM.php"; 
+
+require_once "Controladores/pacientesC.php";
+require_once "Modelos/pacientesM.php"; 
+
+require_once "Controladores/citasC.php";
+require_once "Modelos/citasM.php"; 
+
+require_once "Controladores/adminC.php";
+require_once "Modelos/adminM.php"; 
+
+$plantilla = new Plantilla();
+$plantilla -> LlamarPlantilla();
+
